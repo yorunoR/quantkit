7e93be072c4067cf8b3d230d3e85eee3abe8c8ab
Yoshioka <sss.yoshioka@gmail.com>
2024-04-30 22:20:37 +0900

    set HF_HOME

--- sample.sh
+++ sample.sh
@@ -1,6 +1,13 @@
+sudo sysctl vm.swappiness=10
+
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export HF_HOME=/home/yux/work/infer-with/data
 
 python -m quantkit.cli gptq Sdff-Ltba/LightChatAssistant-2x7B -out LightChatAssistant-2x7B-GPTQ
+# python -m quantkit.cli gptq CohereForAI/c4ai-command-r-plus -out c4ai-command-r-plus-GPTQ
+# python -m quantkit.cli awq CohereForAI/c4ai-command-r-v01 -out c4ai-command-r-v01-AWQ
+# python -m quantkit.cli gptq karakuri-ai/karakuri-lm-70b-chat-v0.1 -out karakuri-lm-70b-chat-v0.1-GPTQ
+# python -m quantkit.cli awq karakuri-ai/karakuri-lm-70b-chat-v0.1 -out karakuri-lm-70b-chat-v0.1-AWQ
 # python -m quantkit.cli gptq Phind/Phind-CodeLlama-34B-v2 -out Phind-CodeLlama-34B-v2-GPTQ
 # python -m quantkit.cli awq Phind/Phind-CodeLlama-34B-v2 -out Phind-CodeLlama-34B-v2-AWQ
 # python -m quantkit.cli gptq CohereForAI/c4ai-command-r-v01 -out c4ai-command-r-v01-GPTQ
